4a7858e821099f01e9d46fd1233b9885cc96decd
Benjamin Strilziw | Paints <b.strilziw@paints.de>
2015-05-07 11:23:58 +0200

    menubar hinzugefügt

--- ImageEditorFrame.java
+++ ImageEditorFrame.java
@@ -8,7 +8,29 @@ import javax.imageio.*;
 public class ImageEditorFrame extends JFrame{ 
 	public ImageEditorFrame(){ 
 		setDefaultCloseOperation(EXIT_ON_CLOSE); 
+		createMenuBar();
 		setSize(400, 300); 
-		setVisible(true); 
-	} 
+		setVisible(true);
+	}
+
+	private void createMenuBar(){ 
+		JMenuBar menuBar = new JMenuBar(); 
+		setJMenuBar(menuBar); 
+		
+		JMenu menuFile = new JMenu("File"); 
+		menuBar.add(menuFile); 
+		
+		JMenuItem menuItemOpen = new JMenuItem("Open"); 
+		menuFile.add(menuItemOpen); 
+		
+		menuItemOpen.addActionListener(new ActionListener() { 
+			public void actionPerformed(ActionEvent e){ 
+				onOpen(); 
+			}
+		}); 
+	}
+
+	private void onOpen(){ 
+		JOptionPane.showMessageDialog(this, "Open Selected"); 
+	}
 }
